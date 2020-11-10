d406076aaf867f1d291261c2d01c56c88a6e8c93
fajar1533 <56420172+fajar1533@users.noreply.github.com>
2020-11-10 10:47:54 +0700

    Add files via upload

---
+++ beranda.php
@@ -0,0 +1,22 @@
+<!doctype html> 
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Judul Halaman Saya</title>
+   <link rel="stylesheet" type="text/css" href="style.css" media="all" />
+  </head>
+<body>
+  <div class="menu-wrap">
+ <ul>
+		<li><a href="beranda.php">Beranda</a></li>
+		<li><a href="barang.php">Barang</a>
+                    <ul>
+                      <li><a href="baru.php">Terbaru</a></li>
+                      <li><a href="laris.php">Terlaris</a></li>
+                    </ul>
+                </li>
+		<li><a href="profil.php">Profil</a></li>
+ </ul>
+  </div>
+</body>
+</html>
